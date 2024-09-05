b06636c5145dd95e044e5a11af0d954fc9cefc8c
Andy Maries <andy.maries@genesys.com>
2024-09-06 09:17:37 +1000

    refactor: Remove unnecessary console.debug statements and update data handling in modificationHandler.js

--- src/modules/pageHandler.js
+++ src/modules/pageHandler.js
@@ -516,13 +516,6 @@ async function loadPageThree() {
 
       // Add event listener for open forecast button
       addEvent(document.getElementById("open-forecast-button"), "click", () => {
-        // Can't seem to make this work easily - button is disabled
-        // temp logging
-        console.log(
-          "[OFG.TEMP] Open forecast button clicked",
-          applicationConfig.outbound.fcUrl
-        );
-
         try {
           // Create an anchor element
           const anchor = document.createElement("a");
